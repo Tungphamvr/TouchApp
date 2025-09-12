9f4d3d378bef2d6648772f31ee1ce54311e1cd6e
TPham <Tung.Pham@soluis.com>
2025-09-13 05:54:41 +0700

    Add Mainmenu

---
+++ Plugins/TungpmLib/Source/TungpmLib/Private/TungpmLibBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TungpmLibBPLibrary.h"
+#include "TungpmLib.h"
+
+UTungpmLibBPLibrary::UTungpmLibBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UTungpmLibBPLibrary::TungpmLibSampleFunction(float Param)
+{
+	return -1;
+}
+
